57637e8f13e3a7e0fee15a8b0edba51b453f283c
Mickalu <45756345+Mickalu@users.noreply.github.com>
2021-03-15 18:01:03 +0100

    function choose pca and see matrixs

---
+++ VeRemi/last_VeReMi/code/PCA_number_component_choice.py
@@ -0,0 +1,20 @@
+import pandas as pd
+from function_PCA import *
+
+
+df = pd.read_csv("D:/School/cours_5eme/projet/code/VeRemi/last_VeReMi/database/cleandataVeReMi.csv")
+liste_number_component = [2,3,4]
+historys = []
+matrixs = []
+
+
+for number in liste_number_component:
+    history, matrix = value_pca_acc(df, number)
+    historys.append(history)
+    matrixs.append(matrix)
+
+accuracy_graph(historys, liste_number_component)
+loss_graph(historys, liste_number_component)
+
+for elem_max in matrixs:
+    print(elem_max)
---
+++ VeRemi/last_VeReMi/code/function_PCA.py
@@ -0,0 +1,75 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import confusion_matrix
+from sklearn.decomposition import PCA
+from keras.models import Sequential
+from keras.layers import Dense
+import pandas as pd
+
+
+def accuracy_graph(historys, liste_number_component):
+    count = 0
+
+    for history in historys:
+        plt.plot(history.history['accuracy'], label = liste_number_component[count])
+        count += 1
+
+    plt.legend(loc = 'upper left')
+    plt.title('model accuracy')
+    plt.ylabel('accuracy')
+    plt.xlabel('epoch')
+
+    plt.show()
+
+
+def loss_graph(historys,liste_number_component):
+    count = 0
+
+    for history in historys:
+        plt.plot(history.history['loss'], label = liste_number_component[count])
+        count += 1
+
+    plt.legend(loc = 'upper left')
+    plt.title('model loss')
+    plt.ylabel('loss')
+    plt.xlabel('epoch')
+
+    plt.show()
+
+def value_pca_acc(df, n_component_pca):
+    X = df.drop(['attackerType'], axis = 1)
+    Y = df['attackerType']
+    Y = pd.get_dummies(Y, columns=['attackerType'])
+
+    X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size = 0.2, random_state = 42)
+
+
+    pca = PCA(n_components= n_component_pca)
+
+    pca.fit(X_train)
+    X_train_pca = pca.transform(X_train)
+    X_test_pca = pca.transform(X_test)
+
+
+
+    model=Sequential()
+
+    model.add(Dense(64, input_shape=(n_component_pca,), activation='relu'))
+    model.add(Dense(32, activation='relu',))
+    model.add(Dense(6, activation='softmax'))
+
+    model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
+
+    
+    history = model.fit(X_train_pca, Y_train, validation_data = (X_test_pca, Y_test), epochs=10)
+
+    Y_predicted = model.predict(X_test_pca)
+    matrix = confusion_matrix_pca(Y_predicted, Y_test)
+    return history, matrix
+
+def confusion_matrix_pca(Y_predicted, Y_test):
+    
+    Y_predicted = (Y_predicted > 0.5) 
+    matrix = confusion_matrix(Y_test.values.argmax(axis=1),Y_predicted.argmax(axis=1))
+    return matrix 
