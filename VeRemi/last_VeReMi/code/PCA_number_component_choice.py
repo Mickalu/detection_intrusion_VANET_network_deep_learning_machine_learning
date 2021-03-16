8cf0b2ca907902a982ca654f29cb4c595c7d4830
Mickalu <45756345+Mickalu@users.noreply.github.com>
2021-03-16 12:20:45 +0100

    add result figure pca component

--- VeRemi/last_VeReMi/code/PCA_number_component_choice.py
+++ VeRemi/last_VeReMi/code/PCA_number_component_choice.py
@@ -1,6 +1,6 @@
 import pandas as pd
 from function_PCA import *
-
+count = 0
 
 df = pd.read_csv("D:/School/cours_5eme/projet/code/VeRemi/last_VeReMi/database/cleandataVeReMi_final.csv")
 liste_number_component = [2,3,4,5,6]
@@ -17,4 +17,5 @@ accuracy_graph(historys, liste_number_component)
 loss_graph(historys, liste_number_component)
 
 for elem_max in matrixs:
-    print(elem_max)
+    print(liste_number_component[count]," : " , elem_max)
+    count += 1
