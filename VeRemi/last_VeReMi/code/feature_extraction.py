d8b930073b1892b07bfbd7a7cb31d0342404ea77
Thomas Bongibault <thomas.bongibault@esme.fr>
2021-03-29 15:55:12 +0200

    add csv file SMOTE

--- VeRemi/last_VeReMi/code/feature_extraction.py
+++ VeRemi/last_VeReMi/code/feature_extraction.py
@@ -3,19 +3,23 @@
 ## Import
 import os
 import pandas as pd
+import numpy as np
 import matplotlib.pyplot as plt
-from matplotlib.pyplot import figure
 import seaborn as sns
 
 from imblearn.over_sampling import SMOTE
 from imblearn.under_sampling import RandomUnderSampler
 from imblearn.pipeline import Pipeline
 from sklearn.preprocessing import MinMaxScaler
+from sklearn.decomposition import PCA
+
+from sklearn.preprocessing import LabelEncoder
+from keras.utils import to_categorical
 
 from sklearn.model_selection import train_test_split
 from keras.models import Sequential
-from keras.layers import Dense, Dropout
-from sklearn.metrics import classification_report,multilabel_confusion_matrix, f1_score
+from keras.layers import Dense
+from sklearn.metrics import classification_report, confusion_matrix, f1_score
 
 
 ###############################################################################
@@ -32,59 +36,67 @@ database_dir_path = path[:-4].replace(os.sep, "/") + "database/"
 df = pd.read_csv(database_dir_path + "cleandataVeReMi_with_directory.csv")
 
 ## Feature Selection
-df.drop(["type", "messageID", "pos_z", "spd_z"], axis=1, inplace=True)
+df.drop(["type", "pos_z", "spd_z"], axis=1, inplace=True)
 
 ## Plot to see the repartition of labels
 plt.figure(figsize=(10,6))
-sns.barplot(x=df["attackerType"].unique(), y=df["attackerType"].value_counts().sort_index(), palette="Blues_r")
+sns.barplot(x=df["attackerType"].unique(), 
+            y=df["attackerType"].value_counts().sort_index(), palette="Blues_r")
 plt.xlabel('\nAttacker Type', fontsize=15, color='#2980b9')
 plt.ylabel("\n", fontsize=15, color='#2980b9')
 plt.title("Repartition of attacker type at the beginning\n", fontsize=18, color='#3742fa')
 plt.tight_layout()
 
 ## Group by
-df_group = df.groupby(by=["sender","receiver","attackerType", "simulation_directory"], 
-                      as_index=False).agg({'rcvTime':'mean', 'sendTime':'mean', 'RSSI':'first', 
-                                           'pos_x':'mean', 'pos_y':'mean', 'spd_x':'mean', 'spd_y':'mean',
-                                           'global_pos':'mean', 'global_spd':'mean'})
+# df_group = df.groupby(by=["sender","receiver", "attackerType", "simulation_directory"], 
+#                       as_index=False).agg({'rcvTime':'mean', 'sendTime':'mean', 'RSSI':'mean', 
+#                                            'pos_x':'mean', 'pos_y':'mean', 'spd_x':'mean', 'spd_y':'mean',
+#                                            'global_pos':'mean', 'global_spd':'mean'})
                                            
 ## Drop the simulation_directory column
-df_group.drop("simulation_directory", axis=1, inplace=True)
+df.drop("simulation_directory", axis=1, inplace=True)
 
-## Rearrange the columns order
-df_group = df_group[['sender', 'receiver', 'rcvTime',
-       'sendTime', 'RSSI', 'pos_x', 'pos_y', 'spd_x', 'spd_y', 'global_pos',
-       'global_spd', 'attackerType']]
+## PCA
+X_pos = df[["sender", "receiver", "sendTime", "rcvTime", "pos_x", "pos_y"]]
+X_spd = df[["sender", "receiver", "sendTime", "rcvTime", "spd_x", "spd_y"]]
 
-## Plot to see the repartition of labels
-plt.figure(figsize=(10,6))
-sns.barplot(x=df_group["attackerType"].unique(), y=df_group["attackerType"].value_counts().sort_index(), palette="Blues_r")
-plt.xlabel('\nAttacker Type', fontsize=15, color='#2980b9')
-plt.ylabel("\n", fontsize=15, color='#2980b9')
-plt.title("Repartition of attacker type with group_by\n", fontsize=18, color='#3742fa')
-plt.tight_layout()
+pca_pos = PCA(n_components=1)
+X_pca_pos = pca_pos.fit_transform(X_pos)
+pca_pos_df = pd.DataFrame(data = X_pca_pos, columns = ['PCA_pos'])
+
+pca_spd = PCA(n_components=1)
+X_pca_spd = pca_spd.fit_transform(X_spd)
+pca_spd_df = pd.DataFrame(data = X_pca_spd, columns = ['PCA_spd'])
+
+df = pd.concat([df, pca_pos_df, pca_spd_df], axis=1)
 
 ## Sampling strategy with SMOTE and RandomUnderSampling
-X_group = df_group.drop("attackerType", axis=1)
-y_group = df_group["attackerType"]
+X = df.drop("attackerType", axis=1)
+y = df["attackerType"]
 
-number_strategy_smote = int((y_group.value_counts().max() * 0.5).round())
+# number_strategy_smote = int((y_group.value_counts().max() * 0.5).round())
 
-strategy = {1:number_strategy_smote, 2:number_strategy_smote, 4:number_strategy_smote, 8:number_strategy_smote, 16:number_strategy_smote}
-oversample = SMOTE(sampling_strategy=strategy, random_state=42)
-undersample = RandomUnderSampler(random_state=42)
+# strategy = {1:number_strategy_smote, 2:number_strategy_smote, 4:number_strategy_smote, 
+#             8:number_strategy_smote, 16:number_strategy_smote}
+# oversample = SMOTE(sampling_strategy=strategy, random_state=42)
+# undersample = RandomUnderSampler(random_state=42)
 
-steps = [("o", oversample), ("u", undersample)]
-pipeline = Pipeline(steps=steps)
+# steps = [("o", oversample), ("u", undersample)]
+# pipeline = Pipeline(steps=steps)
 
-X_sample, y_sample = pipeline.fit_resample(X_group,y_group)
+# X_sample, y_sample = pipeline.fit_resample(X_group,y_group)
+
+oversample = SMOTE(random_state=42)
+X_sample, y_sample = oversample.fit_resample(X,y)
+
 
 ## Concatenate the new data
 df_sample = pd.concat([X_sample, y_sample], axis=1)
 
 ## Plot to see the repartition of labels
 plt.figure(figsize=(10,6))
-sns.barplot(x=df_sample["attackerType"].unique(), y=df_sample["attackerType"].value_counts().sort_index(), palette="Blues_r")
+sns.barplot(x=df_sample["attackerType"].unique(), 
+            y=df_sample["attackerType"].value_counts().sort_index(), palette="Blues_r")
 plt.xlabel('\nAttacker Type', fontsize=15, color='#2980b9')
 plt.ylabel("\n", fontsize=15, color='#2980b9')
 plt.title("Repartition of attacker type after sampling\n", fontsize=18, color='#3742fa')
@@ -96,21 +108,41 @@ for col in list_columns_to_normalize:
     scaler = MinMaxScaler()
     df_sample[col] = scaler.fit_transform(df_sample[[col]])
 
-## Create One Hot Encoding
-df_sample = pd.concat([df_sample,pd.get_dummies(df_sample['attackerType'], prefix='attackerType')],axis=1).drop(['attackerType'],axis=1)
-    
+## Rearrange the columns order
+df_sample = df_sample[['sender', 'receiver', 'sendTime', 'rcvTime', 'messageID', 'RSSI', 'pos_x', 
+         'pos_y', 'spd_x', 'spd_y', 'global_pos','global_spd', 'PCA_pos',
+         'PCA_spd', 'attackerType']]
+
 ## Print correlation matrix
 plt.figure(figsize = (16,10))
 corrMatrix = df_sample.corr()
 ax = sns.heatmap(corrMatrix, annot=True)
 ax.set_xticklabels(ax.get_xticklabels(), rotation=45)
 plt.show()
-                                            
-## Split features and labels
-list_attackerType = ["attackerType_0", "attackerType_1", "attackerType_2", "attackerType_4", "attackerType_8", "attackerType_16"]
-X_sample = df_sample.drop(list_attackerType, axis=1)
-y_sample = df_sample[list_attackerType]
 
+## Save in a csv file                          
+df_sample.to_csv(database_dir_path + "VeReMi_SMOTE.csv", index=False)
+
+## Open csv
+df_sample = pd.read_csv(database_dir_path + "VeReMi_SMOTE.csv")
+
+## Split features and labels
+X_sample = df_sample[["sender", "receiver", "sendTime", "rcvTime", 
+                      "messageID", "RSSI", "PCA_pos", "PCA_spd"]]
+y_sample = df_sample["attackerType"]
+
+## Create One Hot Encoding
+le = LabelEncoder()
+y_le = le.fit_transform(y_sample)
+y_ohe = to_categorical(y_le)
+   
+## Variance with a global PCA to see how n_components to choose
+for n in range(2,8,1):
+    pca = PCA(n_components=n)
+    X_pca = pca.fit_transform(X_sample)
+    explained_variance = pca.explained_variance_
+    print("Variance for",n,"components :",explained_variance)
+## Result : n_components=5
 
 ###############################################################################
 """
@@ -118,28 +150,48 @@ y_sample = df_sample[list_attackerType]
 """
 ###############################################################################
 
-## Train-Test split
-X_train, X_test, y_train, y_test = train_test_split(X_sample, y_sample, test_size = 0.25, shuffle=False, random_state = 42)
+def deep_learning_model(X,y,test_size,first_layer,second_layer,third_layer):
 
+    ## Train-Test split
+    X_train, X_test, y_train, y_test = train_test_split(X, y, 
+                                                    test_size = test_size, 
+                                                    random_state = 42)
 
-## Deep Learning model
-lstm_model = Sequential()
+    ## Deep Learning model
+    model = Sequential()
 
-lstm_model.add(Dense(20, input_shape=(X_sample.shape[1],)))
-lstm_model.add(Dropout(0.2))
-lstm_model.add(Dense(20))
-lstm_model.add(Dropout(0.2))
-lstm_model.add(Dense(10))
-lstm_model.add(Dropout(0.2))
-lstm_model.add(Dense(6, activation='softmax'))
+    model.add(Dense(first_layer, input_shape=(X.shape[1],), activation="tanh"))
+    model.add(Dense(second_layer, activation="tanh"))
+    model.add(Dense(third_layer, activation="tanh"))
+    model.add(Dense(6, activation='softmax'))
 
-lstm_model.compile(loss='binary_crossentropy', optimizer = 'adam', metrics = ['accuracy'])
+    model.compile(loss='categorical_crossentropy', optimizer = 'adam', metrics = ['accuracy'])
 
-lstm_model.fit(X_train, y_train, epochs = 10)
+    model.fit(X_train, y_train, epochs=50, batch_size=4096)
 
-## Prediction and metrics
-y_predicted = lstm_model.predict(X_test)
-y_predicted = y_predicted.round()
-print(multilabel_confusion_matrix(y_test, y_predicted))
-print(classification_report(y_test, y_predicted))
-print(f1_score(y_test, y_predicted, average="macro"))
+    ## Prediction and metrics
+    y_pred = model.predict_classes(X_test)
+    y_pred = le.inverse_transform(y_pred)
+
+    y_test = np.argmax(y_test, axis=1)
+    y_test = le.inverse_transform(y_test)
+
+    print(confusion_matrix(y_test, y_pred))
+    print(classification_report(y_test, y_pred))
+    print("F1 score (macro) :", f1_score(y_test, y_pred, average="macro"))
+    print("Test size :", test_size)
+    print("First layer :", first_layer)
+    print("Second layer :", second_layer)
+    print("Third layer :", third_layer)
+    
+    
+
+test_size_list = [0.2,0.25,0.3]
+    
+## PCA global
+pca = PCA(n_components=5)
+X_pca = pca.fit_transform(X_sample)
+    
+for test_size in test_size_list:
+    deep_learning_model(X_pca,y_ohe,test_size,128,64,32)
+    print("PCA n_components :", 5)
