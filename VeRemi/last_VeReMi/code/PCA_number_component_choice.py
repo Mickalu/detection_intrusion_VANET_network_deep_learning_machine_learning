16e3769ad69ce48be46e68df7f01eaeb19acd5d8
Mickalu <45756345+Mickalu@users.noreply.github.com>
2021-03-29 16:11:45 +0200

    add code test activation

--- VeRemi/last_VeReMi/code/PCA_number_component_choice.py
+++ VeRemi/last_VeReMi/code/PCA_number_component_choice.py
@@ -3,25 +3,23 @@ from function_PCA import *
 import seaborn as sns
 count = 0
 
-df = pd.read_csv("D:/School/cours_5eme/projet/code/VeRemi/last_VeReMi/database/cleandataVeReMi_final_undersampling.csv", index = False)
-liste_number_component = [2,3,4,5,6]
+df = pd.read_csv("D:/School/cours_5eme/projet/code/VeRemi/last_VeReMi/database/cleandataVeReMi_with_directory.csv")
+liste_number_component = [3,4,5,6,7]
 historys = []
 matrixs = []
-list_col_supp = ['type', 'pos_z', 'spd_z', 'Unnamed: 0']
+list_col_supp = ['type', 'pos_z', 'spd_z']
 list_activation_function = ['tanh', 'relu']
 
 df = delete_column_df(df, list_col_supp)
 
-for number in liste_number_component:
-    history, matrix = value_pca_acc(df, number, 'relu')
-    historys.append(history)
-    matrixs.append(matrix)
+for activation in list_activation_function:
+    for number in liste_number_component:
+        history, matrix = value_pca_acc(df, number, activation)
+        historys.append(history)
+        matrixs.append(matrix)
 
-accuracy_graph(historys, liste_number_component)
-loss_graph(historys, liste_number_component)
+    accuracy_graph(historys, liste_number_component)
+    loss_graph(historys, liste_number_component)
 
-for matrix in matrixs:
-    print(matrix)
-    plt.figure(figsize = (10,7))
-    sns.heatmap(matrix, annot=True)
-    count += 1
+    for index_tab in range(len(matrixs)):
+        print(activation, " : \n \n", liste_number_component[index_tab]," : \n ", historys[index_tab].history['accuracy'],"\n", matrixs[index_tab])
